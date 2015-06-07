bb5c6642d12ea94c67f19253fcf93b014e4afcff
Rabosa616 <rabosa@gmail.com>
2015-06-07 13:38:34 +0200

    Using Google Maps Api

---
+++ EjemploGoogleMaps/app/src/main/java/com/example/rabosa/ejemplogooglemaps/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.rabosa.ejemplogooglemaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity   extends FragmentActivity
+        implements GoogleMap.OnMapClickListener {
+    private final LatLng UPV = new LatLng(39.481106, -0.340987);
+    private GoogleMap mapa;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mapa = ((SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(UPV, 15));
+        mapa.setMyLocationEnabled(true);
+        mapa.getUiSettings().setZoomControlsEnabled(false);
+        mapa.getUiSettings().setCompassEnabled(true);
+        mapa.addMarker(new MarkerOptions()
+                .position(UPV)
+                .title("UPV")
+                .snippet("Universidad Politécnica de Valencia")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(android.R.drawable.ic_menu_compass))
+                .anchor(0.5f, 0.5f));
+        mapa.setOnMapClickListener(this);
+    }
+
+    public void moveCamera(View view) {
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(UPV));
+    }
+
+    public void animateCamera(View view) {
+        if (mapa.getMyLocation() != null)
+            mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(mapa.getMyLocation().getLatitude(), mapa.getMyLocation().getLongitude()), 15));
+    }
+
+    public void addMarker(View view) {
+        mapa.addMarker(new MarkerOptions().position(
+                new LatLng(mapa.getCameraPosition().target.latitude,
+                        mapa.getCameraPosition().target.longitude)));
+
+    }
+
+    @Override
+    public void onMapClick(LatLng puntoPulsado) {
+        mapa.addMarker(new MarkerOptions().position(puntoPulsado).
+                icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+    }
+}
