187ee71a44fae8558b8192cb635e8af7a3a67ea4
Rabosa616 <rabosa@gmail.com>
2015-06-07 11:03:43 +0200

    tema 6 ciclo de vida de una app

--- POIS/app/src/main/java/com/example/rabosa/pois/MainActivity.java
+++ POIS/app/src/main/java/com/example/rabosa/pois/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity
         implements AdapterView.OnItemClickListener {
@@ -22,6 +23,7 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_SHORT).show();
         adaptador = new AdaptadorLugares(this);
         /*adaptador = new ArrayAdapter(this,
                 R.layout.elemento_lista,
@@ -31,7 +33,35 @@ public class MainActivity extends Activity
         listView.setAdapter(adaptador);
         listView.setOnItemClickListener(this);
     }
+    @Override protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
 
+    @Override protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+        super.onPause();
+    }
+
+    @Override protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
     @Override
     public void onItemClick(AdapterView parent,View vista,
                                int posicion, long id){
